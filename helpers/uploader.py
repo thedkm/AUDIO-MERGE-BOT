747f097253be492c3e6d616841dbcb808390d18f
Ƭƴɭ៩Ʀ Ɖµɾ∂εɳ <16339128+thedkm@users.noreply.github.com>
2021-11-20 22:50:44 +0530

    Update uploader.py

--- helpers/uploader.py
+++ helpers/uploader.py
@@ -16,7 +16,6 @@ async def uploadVideo(c: Client,cb: CallbackQuery,merged_video_path,video_thumbn
 				chat_id=cb.message.chat.id,
 				audio=merged_video_path,
 				duration=duration,
-				thumb=video_thumbnail,
 				caption=f"**{merged_video_path.rsplit('/',1)[-1]}**",
 				progress=progress_for_pyrogram,
 				progress_args=(
