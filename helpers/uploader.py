e9074bfb304b99c8bc87a9fa23bf7f5ff36ef9c8
Ƭƴɭ៩Ʀ Ɖµɾ∂εɳ <16339128+thedkm@users.noreply.github.com>
2021-11-20 19:11:01 +0530

    Update uploader.py

--- helpers/uploader.py
+++ helpers/uploader.py
@@ -4,6 +4,7 @@ from pyrogram import Client
 from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup,CallbackQuery
 from helpers.display_progress import progress_for_pyrogram,humanbytes
 from config import Config
+from hachoir.metadata import extractMetadata
 
 
 async def uploadVideo(c: Client,cb: CallbackQuery,merged_video_path,width,height,duration,file_size,upload_mode:bool):
@@ -11,11 +12,19 @@ async def uploadVideo(c: Client,cb: CallbackQuery,merged_video_path,width,height
 		sent_ = None
 		if upload_mode is False:
 			c_time = time.time()
+			metadata = extractMetadata(createParser(up_path))
+                        if metadata.has("duration"):
+                        duration = metadata.get('duration').seconds
+                    title = metadata.get("title") if metadata.has("title") else None
+                    artist = metadata.get("artist") if metadata.has("artist") else None
 			sent_ = await c.send_audio(
 				chat_id=cb.message.chat.id,
 				audio=merged_video_path,
 				duration=duration,
+				caption=cap_mono,
 				parse_mode="html",
+				performer=artist,
+                                title=title,
 				caption=f"**File Name: {merged_video_path.rsplit('/',1)[-1]}**",
 				progress=progress_for_pyrogram,
 				progress_args=(
