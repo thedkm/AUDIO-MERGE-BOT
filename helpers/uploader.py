08e4d3360297ca28ff319fa3442319b27eaab2fd
Akshett Rai Jindal <akshett.jindal@research.iiit.ac.in>
2021-11-24 13:23:22 +0530

    Update helper/uploader.py
    Change all whitespaces and underscores to dots in uploaded files
    Remove non-used imports

--- helpers/uploader.py
+++ helpers/uploader.py
@@ -1,22 +1,32 @@
 import time
-import asyncio
 from pyrogram import Client
-from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup,CallbackQuery
-from helpers.display_progress import progress_for_pyrogram,humanbytes
-from config import Config
-from hachoir.metadata import extractMetadata
+from pyrogram.types import CallbackQuery
+from helpers.display_progress import progress_for_pyrogram
+import os
+import re
 
 
 async def uploadVideo(c: Client,cb: CallbackQuery,merged_video_path,video_thumbnail,width,height,duration,file_size,upload_mode:bool):
 	try:
 		sent_ = None
+
+		# Temporary hack for uploading everything as audio irrespective
+		# of the user settings.
+		# TODO: Remove the setting completely from the bot.
+		upload_mode = False
+
+		audio_file_name = os.path.basename(merged_video_path)
+		audio_file_name = re.sub(r"\s\s*", ".", audio_file_name)
+		audio_file_name = re.sub(r"__*", ".", audio_file_name)
+
 		if upload_mode is False:
 			c_time = time.time()
 			sent_ = await c.send_audio(
 				chat_id=cb.message.chat.id,
 				audio=merged_video_path,
 				duration=duration,
-				caption=f"**{merged_video_path.rsplit('/',1)[-1]}**",
+				caption=f"**{audio_file_name}**",
+				file_name=audio_file_name,
 				progress=progress_for_pyrogram,
 				progress_args=(
 					"Uploading file as audio",
