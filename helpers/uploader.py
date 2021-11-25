6d560cdebd31cc602b81485893623f33b344ea41
Ƭƴɭ៩Ʀ Ɖµɾ∂εɳ <16339128+thedkm@users.noreply.github.com>
2021-11-25 13:48:14 +0530

    Update uploader.py

--- helpers/uploader.py
+++ helpers/uploader.py
@@ -6,7 +6,7 @@ import os
 import re
 
 
-async def uploadVideo(c: Client,cb: CallbackQuery,merged_video_path,video_thumbnail,title,performer,duration,file_size,upload_mode:bool):
+async def uploadVideo(c: Client,cb: CallbackQuery,merged_video_path,video_thumbnail,title,artist,duration,file_size,upload_mode:bool):
 	try:
 		sent_ = None
 
@@ -27,7 +27,7 @@ async def uploadVideo(c: Client,cb: CallbackQuery,merged_video_path,video_thumbn
 				chat_id=cb.message.chat.id,
 				audio=merged_video_path,
 				duration=duration,
-                performer=performer,
+                artist=artist,
                 title=re.sub(r"__*", ".", title),
 				caption=f"**{file_caption}**",
 				file_name=audio_file_name,
