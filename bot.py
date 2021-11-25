76255c220d0a5039acbf38c04e4881897a71269c
Ƭƴɭ៩Ʀ Ɖµɾ∂εɳ <16339128+thedkm@users.noreply.github.com>
2021-11-25 18:54:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -544,8 +544,8 @@ async def mergeNow(c:Client, cb:CallbackQuery,new_file_name: str):
 			duration = metadata.get("duration").seconds
 		if metadata.has("title"):
 			title = metadata.get("title")
-		if metadata.has("artist"):
-			artist = metadata.get("artist")
+		if metadata.has("auther"):
+			artist = metadata.get("auther")
     
         
 	except:
