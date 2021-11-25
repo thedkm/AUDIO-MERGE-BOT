22386ab333cd2981e2f7a48d7e7012891e0f9169
Ƭƴɭ៩Ʀ Ɖµɾ∂εɳ <16339128+thedkm@users.noreply.github.com>
2021-11-25 12:58:37 +0530

    Update uploader.py

--- helpers/uploader.py
+++ helpers/uploader.py
@@ -20,6 +20,8 @@ async def uploadVideo(c: Client,cb: CallbackQuery,merged_video_path,video_thumbn
 		audio_file_name = re.sub(r"__*", ".", audio_file_name)
 
 		file_caption = ".".join(audio_file_name.split(".")[:-1])
+        title = re.sub(r"\s\s*", ".", title)
+        title = re.sub(r"__*", ".", title)
 
 		if upload_mode is False:
 			c_time = time.time()
@@ -28,7 +30,7 @@ async def uploadVideo(c: Client,cb: CallbackQuery,merged_video_path,video_thumbn
 				audio=merged_video_path,
 				duration=duration,
                 performer=artist,
-                title=re.sub(r"\s\s*", ".", title),
+                title=title,
 				caption=f"**{file_caption}**",
 				file_name=audio_file_name,
 				progress=progress_for_pyrogram,
