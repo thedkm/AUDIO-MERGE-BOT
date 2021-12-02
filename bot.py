25662beb06e7efc79517d3c712f6fe10c81fab06
Ƭƴɭ៩Ʀ Ɖµɾ∂εɳ <16339128+thedkm@users.noreply.github.com>
2021-12-02 20:18:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -120,7 +120,7 @@ async def start_handler(c: Client, m: Message):
 	)
 
 	
-@mergeApp.on_message((filters.document | filters.Audio) & filters.private & ~filters.edited)
+@mergeApp.on_message((filters.document | filters.audio) & filters.private & ~filters.edited)
 async def Audio_handler(c: Client, m: Message):
 	if await database.allowedUser(uid=m.from_user.id) is False:
 		res = await m.reply_text(
