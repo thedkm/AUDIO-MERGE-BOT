243aedfec3c7cb5593aa74d27d8aa42b1a3baf61
Ƭƴɭ៩Ʀ Ɖµɾ∂εɳ <16339128+thedkm@users.noreply.github.com>
2021-12-05 12:45:36 +0530

    Revert "Update bot.py"
    This reverts commit 1ec8784132367bd79d0d4ceb0db99490f09440ca.

--- bot.py
+++ bot.py
@@ -327,7 +327,7 @@ async def callback(c: Client, cb: CallbackQuery):
 				new_file_name = f"./downloads/{str(cb.from_user.id)}/{ascii_.replace(' ', '_')}.mp3"
 				await mergeNow(c,cb,new_file_name)
 		if 'NO' in cb.data:
-			await mergeNow(c,cb,new_file_name = f"./downloads/{str(cb.from_user.id)}/merged.mp3")
+			await mergeNow(c,cb,new_file_name = f"./downloads/{str(cb.from_user.id)}/{str(title)}.mp3")
 
 	elif cb.data == 'cancel':
 		await delete_all(root=f"downloads/{cb.from_user.id}/")
